8e9281f0f54fbd384e1ff294c2d636ffc48ad4e8
Aran Deltac <bluefeet@gmail.com>
2012-04-08 17:29:09 -0700

    Add copyright and license info.

--- lib/MooseX/Types/Data/Serializer.pm
+++ lib/MooseX/Types/Data/Serializer.pm
@@ -111,3 +111,14 @@ if ( !$@ ) {
 }
 
 1;
+__END__
+
+=head1 AUTHOR
+
+Aran Clary Deltac <bluefeet@gmail.com>
+
+=head1 LICENSE
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
